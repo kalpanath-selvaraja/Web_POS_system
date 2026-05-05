4d6c5a119f56bde4aa413d351edff180993996a3
Kalpanath <kalpanathofficial@gmail.com>
2026-05-05 08:11:49 +0530

    add new Items

--- db/database.js
+++ db/database.js
@@ -3,6 +3,22 @@ let customer_db = [
         { id: 'C001', name: 'Nirmal Pedrera',  phone: '0123456789', email: 'nirmal@gmail.com',  address: 'Colombo' },
         { id: 'C002', name: 'Ayesha Montoya',  phone: '9876543210', email: 'ayesha@gmail.com',  address: 'Kandy' },
         { id: 'C003', name: 'Ravi Singh',      phone: '1234567890', email: 'ravi@gmail.com',    address: 'Galle' },
+        { id: 'C004', name: 'Sofia Rodriguez', phone: '2345678901', email: 'sofia@gmail.com',   address: 'Negombo' },
+        { id: 'C001', name: 'Nirmal Pedrera',  phone: '0123456789', email: 'nirmal@gmail.com',  address: 'Colombo' },
+        { id: 'C002', name: 'Ayesha Montoya',  phone: '9876543210', email: 'ayesha@gmail.com',  address: 'Kandy' },
+        { id: 'C003', name: 'Ravi Singh',      phone: '1234567890', email: 'ravi@gmail.com',    address: 'Galle' },
+        { id: 'C004', name: 'Sofia Rodriguez', phone: '2345678901', email: 'sofia@gmail.com',   address: 'Negombo' },
+        { id: 'C001', name: 'Nirmal Pedrera',  phone: '0123456789', email: 'nirmal@gmail.com',  address: 'Colombo' },
+        { id: 'C002', name: 'Ayesha Montoya',  phone: '9876543210', email: 'ayesha@gmail.com',  address: 'Kandy' },
+        { id: 'C003', name: 'Ravi Singh',      phone: '1234567890', email: 'ravi@gmail.com',    address: 'Galle' },
+        { id: 'C004', name: 'Sofia Rodriguez', phone: '2345678901', email: 'sofia@gmail.com',   address: 'Negombo' },
+        { id: 'C001', name: 'Nirmal Pedrera',  phone: '0123456789', email: 'nirmal@gmail.com',  address: 'Colombo' },
+        { id: 'C002', name: 'Ayesha Montoya',  phone: '9876543210', email: 'ayesha@gmail.com',  address: 'Kandy' },
+        { id: 'C003', name: 'Ravi Singh',      phone: '1234567890', email: 'ravi@gmail.com',    address: 'Galle' },
+        { id: 'C004', name: 'Sofia Rodriguez', phone: '2345678901', email: 'sofia@gmail.com',   address: 'Negombo' },
+        { id: 'C001', name: 'Nirmal Pedrera',  phone: '0123456789', email: 'nirmal@gmail.com',  address: 'Colombo' },
+        { id: 'C002', name: 'Ayesha Montoya',  phone: '9876543210', email: 'ayesha@gmail.com',  address: 'Kandy' },
+        { id: 'C003', name: 'Ravi Singh',      phone: '1234567890', email: 'ravi@gmail.com',    address: 'Galle' },
         { id: 'C004', name: 'Sofia Rodriguez', phone: '2345678901', email: 'sofia@gmail.com',   address: 'Negombo' }
     ];
 
@@ -14,4 +30,8 @@ let Item_db = [
     { id: 'I004', name: 'T-Shirt', price: 1200, qty: 20, category: 'Clothing' }
 ]
 
-export {customer_db,Item_db};
+
+let order_db = [];
+
+
+export {customer_db,Item_db,order_db};
