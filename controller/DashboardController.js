cafc2ff930d0a2148a2eafee160f5345e74efea6
Kalpanath <kalpanathofficial@gmail.com>
2026-05-05 07:33:36 +0530

    update Dashboard details load

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -1,46 +1,52 @@
 import {customer_db} from "../db/database.js";
+import {getOrderData, getRecentOrders, getTotalOrders, getTotalRevenue} from "../model/OrderModel.js";
+
+// update the dashbord details
+function updateStats() {
+    $("#stat_customers").text(customer_db.length);
+    $("#stat_total_orders").text(getTotalOrders());
+    $("#stat_revenue").text(getTotalRevenue().toFixed(2));
+
+
+}
+
+// loard dahbord
+const loadTable = () => {
+    $('#dashboard_tbody').empty();
+
+    getRecentOrders().forEach((order, index) => {
+        let row = `
+        <tr data-index="${index}">
+            <td>${order.order_id}</td>
+            <td>${order.customer_name}</td>
+            <td>${order.total.toFixed(2)}</td>
+            <td>${order.date}</td>
+        </tr>
+        `;
+        $('#dashboard_tbody').append(row);
+    });
+}
+
 
 $(document).ready(function () {
 
-    function updateStats() {
-        $("#stat_customers").text(customer_db.length);
-        console.log(customer_db.length);
-    }
-
-    const loadTable = () => {
-
-        $('#dashboard_table_body').empty();
-
-        customer_db.forEach((item , index) => {
-            let new_row =
-                `<tr data-index="${index}"> 
-                    <td>${item.id}</td>
-                    <td>${item.name}</td>
-                    <td>${item.phone}</td>
-                    <td>${item.address}</td>
-                    <td class="text-end">
-                    <button class="btn btn-outline-primary">Edit</button>
-                    <button class="btn btn-outline-danger">Delete</button>
-                    </td>
-                </tr>
-                    `;
-
-
-
-            $('#dashboard_tbody').append(new_row);
-        })
-    }
-
+    // greetings
     function greetings() {
-        $('#greetings').text(sessionStorage.getItem("username"));
+        $('#greetings').text("Welcome Back, " + sessionStorage.getItem("username"));
     }
-
 
 
     greetings();
     updateStats();
     loadTable();
 
-
-
 });
+
+updateStats();
+
+
+// load the dashdoard content at start , must be used when login so it must be exported
+export const loadDashboard = () => {
+    updateStats();
+    loadTable();
+}
